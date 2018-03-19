72e0b8f6d5495a1503849571c758095bf900f952
Iwona Strubczewska <iwona.strubczewska@gmail.com>
2018-03-19 16:18:14 +0100

    Logs

---
+++ TestsAndLogs/src/main/java/Project/GUI.java
@@ -0,0 +1,318 @@
+package Project;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import org.apache.logging.log4j.core.appender.FileAppender;
+
+public class GUI extends Application implements EventHandler<ActionEvent> {
+
+
+    private Button b1, b2, b3, b4, b5, b6, b7;
+    private BankImpl bank = new BankImpl();
+    private VBox topMenu = new VBox();
+    private Label label = new Label();
+    private Label warlab = new Label();
+    private TextField text1 = new TextField();
+    private TextField text2 = new TextField();
+    private TextField text3 = new TextField();
+
+    private static final Logger logger = LogManager.getLogger(GUI.class);
+
+
+
+    public static void main(String[] args) throws IOException {
+        logger.info("Uruchomienie aplikacji");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Title");
+
+        b1 = new Button("Znajdź konto");
+        b2 = new Button("Utwórz konto");
+        b3 = new Button("Wypłać kwotę");
+        b4 = new Button("Wpłać kwotę");
+        b5 = new Button("Przelej kwotę");
+        b6 = new Button("Sprawdz stan konta");
+        b7 = new Button("Gotowe");
+
+        b1.setPrefWidth(1000);
+        b2.setPrefWidth(1000);
+        b3.setPrefWidth(1000);
+        b4.setPrefWidth(1000);
+        b5.setPrefWidth(1000);
+        b6.setPrefWidth(1000);
+        b7.setPrefWidth(1000);
+
+        topMenu.getChildren().addAll(b1,b2,b3,b4,b5,b6);
+        String message;
+
+        BorderPane borderpane = new BorderPane();
+        borderpane.setCenter(topMenu);
+
+
+
+
+        b1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                clear();
+                if (event.getSource() == b1) {
+
+                    logger.info("wybranie opcji szukania konta");
+                    String message = new String("wprowadz nazwe konta oraz adres");
+                    label.setText(message);
+                    topMenu.getChildren().addAll(label, text1, text2,b7);
+
+                    b7.setOnAction(e->
+                    {
+                        clear();
+
+                        if((bank.findAccount(text1.getText(),text2.getText()))!=null) {
+                            String message2=new String("znaleziono konto");
+                            warlab.setText(message2);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("znaleziono konto");
+
+                        }
+                        else {
+                            String message2=new String("nie znaleziono konta");
+                            warlab.setText(message2);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("nie znaleziono konta");
+                        }
+                    });
+                }
+            }
+        });
+
+        b2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                clear();
+
+                if (event.getSource() == b2) {
+                    logger.info("wybranie opcji tworzenia konta");
+
+                    String message = new String("wprowadz nazwe konta oraz adres");
+                    label.setText(message);
+                    clear();
+                    topMenu.getChildren().addAll(label, text1, text2,b7);
+
+
+
+                    b7.setOnAction(e->
+                    {
+                        clear();
+
+                        if((bank.createAccount(text1.getText(),text2.getText()))!=null) {
+                            String message2=new String("utworzono konto");
+                            warlab.setText(message2);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("utworzono konto");
+                        }
+                    });
+                }
+            }
+        });
+
+
+        b3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                clear();
+
+                if (event.getSource() == b3) {
+                    logger.info("wybranie opcji wypłaty gotówki");
+
+                    String message = new String("Wprowadz id konta oraz kwotę");
+                    label.setText(message);
+                    topMenu.getChildren().addAll(label, text1, text2,b7);
+
+                    b7.setOnAction(e->
+                    {
+                        clear();
+                        try {
+                            bank.withdraw(Integer.parseInt(text1.getText()), Long.parseLong(text2.getText()));
+                            String message1=new String("Wypłacono");
+                            warlab.setText(message1);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("wypłacono gotówkę");
+
+                        } catch (Bank.InsufficientFundsException ex) {
+                            String war = new String ("Brak wystarczajacych środków na koncie");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak wystarczających środków na koncie, operacja przerwana");
+                        }catch ( Bank.AccountIdException ex){
+                            String war = new String ("Brak konta o podanym id");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak konta o podanym id, operacja przerwana");
+                        }catch ( NumberFormatException ex) {
+                            String war = new String("Wprowadzono niepoprawny format danych ");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Wprowadzono niepoprawny format danych ");
+                        }
+
+                    });
+                }
+            }
+        });
+
+        b4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                clear();
+
+                if (event.getSource() == b4) {
+                    logger.info("wybranie opcji wpłaty gotówki");
+
+                    String message = new String("Wprowadz id konta oraz kwotę");
+                    label.setText(message);
+                    topMenu.getChildren().addAll(label, text1, text2,b7);
+
+                    b7.setOnAction(e->
+                    {
+                        clear();
+                        try {
+                            bank.deposit(Integer.parseInt(text1.getText()), Long.parseLong(text2.getText()));
+                            String message1=new String("Na konto wpłynęła nowe środki");
+                            warlab.setText(message1);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("Na konto wpłynęły nowe srodki");
+                        } catch ( Bank.AccountIdException ex){
+                            String war = new String ("Brak konta o podanym id");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak konta o podanym id, operacja przerwana");
+                        }catch ( NumberFormatException ex) {
+                            String war = new String("Wprowadzono niepoprawny format danych ");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Wprowadzono niepoprawny format danych ");
+                        }
+                    });
+                }
+            }
+        });
+
+        b5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                clear();
+                if (event.getSource() == b5) {
+                    logger.info("wybranie opcji przelew");
+                    String message = new String("Wprowadz id kont oraz kwotę");
+                    label.setText(message);
+                    topMenu.getChildren().addAll(label, text1, text2, text3, b7);
+
+                    b7.setOnAction(e-> {
+                        clear();
+                        try {
+                            bank.transfer(Integer.parseInt(text1.getText()),Integer.parseInt(text2.getText()),Long.parseLong(text3.getText()));
+                            String message2=new String("Operacja przebiegła pomyślnie");
+                            warlab.setText(message2);
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("Operacja przebiegła pomyślnie");
+                        } catch ( Bank.AccountIdException ex){
+                            String war = new String ("Brak konta o podanym id");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak konta o podanym id, operacja przerwana");
+                        } catch (Bank.InsufficientFundsException ex){
+                            String war = new String ("Brak wystarczających środków na koncie");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak wystarczających środków na koncie, operacja przerwana");
+                        }catch ( NumberFormatException ex) {
+                            String war = new String("Wprowadzono niepoprawny format danych ");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Wprowadzono niepoprawny format danych ");
+                        }
+                    });
+                }
+            }
+        });
+
+
+        b6.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                clear();
+                if (event.getSource() == b6) {
+                    logger.info("wybranie opcji sprawdź stan konta");
+                    String message = new String("Wprowadz id konta");
+                    clear();
+                    label.setText(message);
+                    topMenu.getChildren().addAll(label, text1, b7);
+
+                    b7.setOnAction(e-> {
+                        clear();
+                        try {
+                            Integer amount = new Integer(Integer.parseInt(text1.getText()));
+                            warlab.setText(Long.toString(bank.getBalance(amount)));
+                            topMenu.getChildren().add(warlab);
+                            logger.debug("Operacja przebiegła pomyślnie");
+                        } catch ( Bank.AccountIdException ex){
+                            String war = new String ("Brak konta o podanym id");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Brak konta o podanym id, operacja przerwana");
+                        }catch ( NumberFormatException ex) {
+                            String war = new String("Wprowadzono niepoprawny format danych ");
+                            warlab.setText(war);
+                            topMenu.getChildren().add(warlab);
+                            logger.warn("Wprowadzono niepoprawny format danych ");
+                        }
+                    });
+                }
+            }
+        });
+
+
+        Scene scene = new Scene(borderpane, 400, 350);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    private void clear(){
+
+        topMenu.getChildren().remove(text1);
+        topMenu.getChildren().remove(text2);
+        topMenu.getChildren().remove(text3);
+        topMenu.getChildren().remove(label);
+        topMenu.getChildren().remove(warlab);
+        topMenu.getChildren().remove(b7);
+
+    }
+    @Override
+    public void handle(ActionEvent event) {
+
+
+
+    }
+
+}
+
